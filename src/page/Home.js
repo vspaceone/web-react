5acb109dc5347b5f0f24f6257138e52647da027c
Damian Jesionek <damian@jesionek.dev>
2023-02-12 00:07:41 +0100

    Reordered images

--- src/page/Home.js
+++ src/page/Home.js
@@ -131,34 +131,34 @@ class HomeInfoBoxBanner extends Component {
 
         const slideContent = [
             {
-                    title: "Ausstattung",
-                    text: "Wir haben drei moderne Räume. In der Brücke stehen bequeme Sofas, ein Beamer und ein Kühlschrank. Im Maschinenraum, kann an dank unserer vieler Maschinen an größeren Projekten gearbeitet werden. Das Labor ist mit einem großzügigen Elektronikarbeitsplatz ausgestattet.",
-                    imageSrc: "pic/vspaceone_maschinenraum.jpg"
+                title: "Gemeinsam zum Ziel",
+                text: "Sich gegenseitig motivieren, inspirieren, helfen und voneinander lernen. Manches macht gemeinsam einfach mehr Spaß!",
+                imageSrc: "pic/vspaceone_gemeinschaft.jpg"
             },
             {
-                    title: "3D Druck",
-                    text: "Unser neuer Prusa i3 MK3s liefert dank zahlreicher Voreinstellungen und ausgeklügelter Features selbst nach kurzen Einweisungen in schnellster Zeit zu Ergebnissen die sich sehen lassen können.",
-                    imageSrc: "pic/vspaceone_prusa_mk3s_small.jpg"
+                title: "3D Druck",
+                text: "Unser neuer Prusa i3 MK3s liefert dank zahlreicher Voreinstellungen und ausgeklügelter Features selbst nach kurzen Einweisungen in schnellster Zeit zu Ergebnissen die sich sehen lassen können.",
+                imageSrc: "pic/vspaceone_prusa_mk3s_small.jpg"
             },
             {
-                    title: "Elektronik",
-                    text: "Der Elektronikarbeitsplatz lädt zum Hacken ein. Stets zur Hand sind Lötkolben, Heißluftstation, Multimeter, Labornetzteile, digitales Oszilloskop sowie ein Haufen Zubehör und natürlich Kabel.",
-                    imageSrc: "pic/vspaceone_big_psu_small.jpg"
+                title: "Elektronik",
+                text: "Der Elektronikarbeitsplatz lädt zum Hacken ein. Stets zur Hand sind Lötkolben, Heißluftstation, Multimeter, Labornetzteile, digitales Oszilloskop sowie ein Haufen Zubehör und natürlich Kabel.",
+                imageSrc: "pic/vspaceone_big_psu_small.jpg"
             },
             {
-                    title: "Reparatur",
-                    text: "Eine Wand voll mit sortierten Elektronikkomponenten bietet die Qual der Wahl von üblicherweise benötigen Bauteile wie Widerstände oder Kondensatoren.",
-                    imageSrc: "pic/vspaceone_drawer_wall_small.jpg"
+                title: "Reparatur",
+                text: "Eine Wand voll mit sortierten Elektronikkomponenten bietet die Qual der Wahl von üblicherweise benötigen Bauteile wie Widerstände oder Kondensatoren.",
+                imageSrc: "pic/vspaceone_drawer_wall_small.jpg"
             },
             {
-                    title: "Holz und Metall",
-                    text: "Unsere noch nicht allzu große, aber ständig wachsende, Ausstattung an Werkzeug und Maschinen steht jederzeit für große und kleine Projekte in unseren Räumen bereit.",
-                    imageSrc: "pic/vspaceone_holzundmetall.jpg"
+                title: "Holz und Metall",
+                text: "Unsere noch nicht allzu große, aber ständig wachsende, Ausstattung an Werkzeug und Maschinen steht jederzeit für große und kleine Projekte in unseren Räumen bereit.",
+                imageSrc: "pic/vspaceone_holzundmetall.jpg"
             },
             {
-                    title: "Gemeinsam zum Ziel",
-                    text: "Sich gegenseitig motivieren, inspirieren, helfen und voneinander lernen. Manches macht gemeinsam einfach mehr Spaß!",
-                    imageSrc: "pic/vspaceone_gemeinschaft.jpg"
+                title: "Ausstattung",
+                text: "Wir haben drei moderne Räume. In der Brücke stehen bequeme Sofas, ein Beamer und ein Kühlschrank. Im Maschinenraum, kann an dank unserer vieler Maschinen an größeren Projekten gearbeitet werden. Das Labor ist mit einem großzügigen Elektronikarbeitsplatz ausgestattet.",
+                imageSrc: "pic/vspaceone_maschinenraum.jpg"
             }
         ]
 
